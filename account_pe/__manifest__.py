7f105634dd9522958046368bbb22eb9d1ee49e66
JorgeTelloH <41442687+JorgeTelloH@users.noreply.github.com>
2021-10-26 16:38:59 -0500

    carga inicial

--- account_pe/__manifest__.py
+++ account_pe/__manifest__.py
@@ -1,52 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Localización para Perú v11",
-    'summary': """Localiza Odoo para Perú v11""",
-
-    'description': """Instala dependencias, plan de cuentas y modificaciones""",
-
-    'author': "Jose Balbuena A. (Cabalcon S.A.C.)",
-    'website': "http://www.cabalcon.com.pe",
-    'category': "Customization",
-    'version': "1.0",
-
-    # any module necessary for this one to work correctly
-    'depends': [
-        'base',
-        'contacts',
-        'account',
-        'l10n_pe',
-        'account_tax_python',
-        'odoope_einvoice_base',
-        'account_invoice_dates',
-        'account_group_menu',
-        'validation_ruc_dni',
-        'odoope_currency',
-        ],
-
-    # always loaded
+    'name': 'Account normalice peruvian',
+    'description': """
+	Arreglos en contabilidad para peru
+    """,
+    'author': "Oswaldo Lopez S. (Cabalcon S.A.C.)",
+    'category': 'Accounting',
+    'depends': ['account','l10n_pe_edi'],
     'data': [
         'security/ir.model.access.csv',
-        'views/partner_view.xml',
-        'views/account_view_pe.xml',
-        'views/account_invoice_views_pe.xml',
-        'views/account_payment_view_pe.xml',
-        'wizard/account_invoice_refund_view.xml',
-        'data/ir.sequence.xml',
-        'data/account.journal.xml',
-        ],
-
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-        ],
-    
-    'post_init_hook': "post_init_hook",
+        'views/account_move_view.xml',
+        'views/account_journal.xml',
+        'views/l10n_latan_document_type_view.xml',
+    ],
+    'application': False,
+    'auto_install': False
 }
-
-
-
-
-
-
-
--- account_pe/models/__init__.py
+++ account_pe/models/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import account_pe
-from . import partner
-from . import account_move_pe
-from . import account_invoice_pe
-from . import account_payment_pe
+from . import account_move
+from . import account_journal
+from . import invoice_payment
